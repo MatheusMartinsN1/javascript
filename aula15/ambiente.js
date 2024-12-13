fffc7e767f6ff5cf6ee05000c961188bd27fa2ab
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2024-12-12 21:34:54 -0300

    aula de arrays

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8 , 2, 9, 3]
+num.push(4)
+num.sort()
+console.log(`O array tem ${num} valores`)
+console.log(`O array tem ${num.length} posições`) 
+console.log(`O segundo valor do array é ${num[1]}`)
+let pos = num.indexOf(9)
+console.log(`O valor 9 está na posição ${pos}`)
